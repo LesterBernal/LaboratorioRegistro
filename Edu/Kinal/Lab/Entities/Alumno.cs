32bfaa11468d59d76503b2dc87865fb3a70bc93a
LesterBernal <101464420+LesterBernal@users.noreply.github.com>
2022-03-19 03:05:58 -0600

    Update Alumno.cs

--- Edu/Kinal/Lab/Entities/Alumno.cs
+++ Edu/Kinal/Lab/Entities/Alumno.cs
@@ -1,38 +1,36 @@
 using LaboratorioRegistro.Edu.Kinal.Lab.Interfaces;
 
-namespace LaboratorioRegistro.Edu.Kinal.Lab.Entities
+namespace LaboratorioRegistro.Edu.Kinal.Lab.Entities;
 
+
+public class Alumno : Persona, IOperaciones
 {
-    public class Alumno : Persona, IOperaciones
+    public string Carne {get;set;}
+    public int NumeroCreditos {get;set;}
+
+    public Alumno() : base()
     {
-        public string Carne { get; set; }
-        public int NumeroCreditos { get; set; }
-
-        public override void TomarAsistencia()
-        {
-            Console.WriteLine($"Asistencia alumno: {this.Apellidos} {this.Nombres} con carné: {this.Carne}");
-        }
-
-        public void ListarMisDatos(string identificador)
-        {
-            Console.WriteLine($"Datos Generales del alumno: UUID: {this.UUID}, Apellidos: {this.Apellidos}, Nombres: {this.Nombres}, Número de Créditos {this.NumeroCreditos}");
-        }
-
-        public bool EliminarAsignatura(string asignatura)
-        {
-            Console.WriteLine($"Se ha eliminado la asignatura del alumno {this.Apellidos} {this.Nombres}, perdera la cantidad de créditos {this.NumeroCreditos}");
-            return true;
-        }
-
-        public Alumno()
-        {
-        }
-
-        public Alumno(string uuid, string apellidos, string nombres, string email, string carne, int numeroCreditos)
+        
+    }
+    public Alumno(string uuid, string apellidos, string nombres, string email, string carne, int numeroCreditos)
         : base(uuid, apellidos, nombres, email)
-        {
-            this.Carne = carne;
-            this.NumeroCreditos = numeroCreditos;
-        }
+    {
+        this.Carne = carne;
+        this.NumeroCreditos = numeroCreditos;
+    }
+    public bool EliminarAsignatura(string asignatura)
+    {
+        Console.WriteLine($"Se ha eliminado la asignatura de {this.Apellidos} {this.Nombres}, Perdera la cantidad de creditos de {this.NumeroCreditos}");
+        return true;
+    }
+
+    public void ListarMisDatos(string identificador)
+    {
+        Console.WriteLine($"{this.Uuid} {this.Apellidos} {this.Nombres} {this.NumeroCreditos}");
+    }
+
+    public override void TomarAsistencia()
+    {
+        Console.WriteLine($"Tomando Asistencia de {this.Apellidos} {this.Nombres}");
     }
 }
