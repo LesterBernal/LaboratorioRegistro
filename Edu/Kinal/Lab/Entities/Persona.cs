88e32807afa79eae4b341a711519078df3462982
LesterBernal <101464420+LesterBernal@users.noreply.github.com>
2022-03-19 03:06:39 -0600

    Update Persona.cs

--- Edu/Kinal/Lab/Entities/Persona.cs
+++ Edu/Kinal/Lab/Entities/Persona.cs
@@ -4,22 +4,20 @@ namespace LaboratorioRegistro.Edu.Kinal.Lab.Entities
 {
     public abstract class Persona
     {
-        public string UUID { get; set; }
-        public string Apellidos { get; set; }
-        public string Nombres { get; set; }
-        public string Email { get; set; }
-
+        public string Uuid {get;set;}
+        public string Apellidos {get;set;}
+        public string Nombres {get;set;}
+        public string Email {get;set;}
         public abstract void TomarAsistencia();
         public Persona()
-        {
+        {            
         }
         public Persona(string uuid, string apellidos, string nombres, string email)
         {
-            this.UUID = uuid;
+            this.Uuid = uuid;
             this.Apellidos = apellidos;
             this.Nombres = nombres;
             this.Email = email;
         }
-
     }
 }
